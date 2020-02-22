9be936a1aeca8b3b3e61ef1ffd125999c37339ba
Ian Walston <ikwalston@comcast.net>
2020-02-21 19:34:14 -0500

    added level3 and level4

--- src/App.js
+++ src/App.js
@@ -8,73 +8,77 @@ import startingballs from "./startingballs.json";
 import levels from "./levels.json";
 
 class App extends React.Component {
-  state = {
-    score: 0,
-    pickedcolors: [],
-    level: 1,
-    balls: startingballs
-  };
+    state = {
+        score: 0,
+        pickedcolors: [],
+        level: 2,
+        balls: startingballs
+    };
 
-  shuffle = () => {
-    //get an array of the 12 positions in random order
-    const randompositions = Object.keys(positions)
-      .map(a => [Math.random(), a])
-      .sort((a, b) => a[0] - b[0])
-      .map(a => a[1]);
+    shuffle = () => {
+        //get an array of the 12 positions in random order
+        const randompositions = Object.keys(positions)
+            .map(a => [Math.random(), a])
+            .sort((a, b) => a[0] - b[0])
+            .map(a => a[1]);
 
-    //mutate copy of balls with random positions
-    const newballs = this.state.balls.map(ball => {
-      ball.loc = positions[randompositions.pop()];
-      return ball;
-    });
-
-    //set state balls to copy
-    this.setState({ balls: newballs });
-  };
-
-  clickball = color => {
-    //determin right from wrong
-    if (this.state.pickedcolors.includes(color)) {
-      alert("you've already picked that color");
-      this.setState({ pickedcolors: [] });
-      this.setState({ score: 0 });
-    } else {
-      this.setState({ pickedcolors: this.state.pickedcolors.concat([color]) });
-      this.setState({ score: this.state.score + 1 });
-      if (this.state.score >= 11) {
-        //set next level
-        alert("great job");
+        //get a copy of balls with random positions
         const newballs = this.state.balls.map(ball => {
-          const newcolor = levels["level" + (this.state.level + 1)].pop();
-
-          ball.color = newcolor;
-          return ball;
+            ball.loc = positions[randompositions.pop()];
+            return ball;
         });
 
-        console.log(newballs);
+        //set state balls to copy
+        this.setState({ balls: newballs });
+    };
 
-        this.setState({ pickedcolors: [] });
-        this.setState({ score: 0 });
-        this.setState({ level: this.state.level + 1 });
-      }
+    clickball = color => {
+        //determin right from wrong
+        if (this.state.pickedcolors.includes(color)) {
+            alert("you've already picked that color");
+            this.setState({
+                pickedcolors: [],
+                score: 0
+            });
+        } else {
+            this.setState({
+                pickedcolors: this.state.pickedcolors.concat([color]),
+                score: this.state.score + 1
+            });
+            if (this.state.score >= 11) {
+                //set next level
+                alert("great job");
+
+                this.setState({
+                    pickedcolors: [],
+                    score: 0,
+                    level: this.state.level + 1
+                });
+                this.state.balls.forEach(ball => {
+                    const newcolor = levels[
+                        "level" + (this.state.level + 1)
+                    ].pop();
+                    ball.color = newcolor;
+                });
+              }
+        }
+
+        this.shuffle();
+    };
+
+    render() {
+        return (
+            <div>
+                <Display score={this.state.score} level={this.state.level} />
+
+                <svg viewBox="0 0 841.9 595.3">
+                    {this.state.balls.map(ball => (
+                        <Circle {...ball} click={this.clickball} />
+                    ))}
+                </svg>
+            </div>
+        );
     }
-
-    this.shuffle();
-  };
-
-  render() {
-    return (
-      <div>
-        <Display score={this.state.score} level={this.state.level} />
-
-        <svg viewBox="0 0 841.9 595.3">
-          {this.state.balls.map(ball => (
-            <Circle {...ball} click={this.clickball} />
-          ))}
-        </svg>
-      </div>
-    );
-  }
 }
 
 export default App;
--- src/components/display.js
+++ src/components/display.js
@@ -3,19 +3,12 @@ import React from "react";
 const Display = props => (
   <div>
     <nav className="navbar navbar-light bg-light">
-      <span>
         <h1>Memory Balls</h1>
-      </span>{" "}
-      <span>
         <h1>Score: {props.score}/12</h1>
-      </span>{" "}
-      <span>
-        <h1>Level: {props.level}</h1>
-      </span>
-      .
+        <h1>Level {props.level}</h1>
     </nav>
     <div className="row justify-content-center">
-      <p> pick each color once</p>
+      <p class='alert alert-info'> pick each color once</p>
     </div>
   </div>
 );
