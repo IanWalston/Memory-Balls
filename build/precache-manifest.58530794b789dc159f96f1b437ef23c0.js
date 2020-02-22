649ab6147296c93d1b7e20718a7f72f59928ddab
Ian Walston <ikwalston@comcast.net>
2020-02-21 19:42:49 -0500

    build folder

---
+++ build/precache-manifest.58530794b789dc159f96f1b437ef23c0.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "4ab0438352810339f522",
+    "url": "./static/js/main.4ab04383.chunk.js"
+  },
+  {
+    "revision": "7ed546fdcde8c9e1e976",
+    "url": "./static/js/1.7ed546fd.chunk.js"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "4715094881abf708730bb083341fcb7d",
+    "url": "./index.html"
+  }
+];
